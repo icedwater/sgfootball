1cacf4aec05bfaf9848f52947b627452bc488f31
Kheng Hui Yeo <icedwater@gmail.com>
2012-09-24 14:53:15 +0800

    Removed unnecessary classes from index.html and functions from main.js.

--- js/main.js
+++ js/main.js
@@ -5,20 +5,7 @@ $(document).ready(function(){
 	// here.
 
 	$("section").on("click", function(){	
-		// $("ul", this).toggleClass("hidden");
-		$("ul", this).fadeToggle(900);
+		$("ul", this).fadeToggle(450);
 	});
 
-	$("img").on("dblclick", function(){
-		console.log("Just once, that's enough.");
-		$("img").width(570);
-		$("h1").fadeIn();
-		$(".booboo").slideDown();
-	});
-
-	$("img").click(function(){
-		$("img").width(200);
-		$("h1").fadeOut();
-		$(".booboo").slideUp();
-	});
 });
