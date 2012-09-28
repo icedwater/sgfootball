7c0d3cdc5bad28c1657d53253327f5d7279bd660
Kheng Hui Yeo <icedwater@gmail.com>
2012-09-28 14:33:05 +0800

    Centered page, tried to get page preview panes working.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,18 @@
 // $("img").width(200);
 
 $(document).ready(function(){
-	// here.
+	// Start with only clubs open
+	$("ul","section").css("display","none");
+	$("ul","#clubs").css("display","block");
 
-	$("section").on("click", function(){	
-		$("ul", this).fadeToggle(450);
+	$("section").on("click", function(e){
+		e.preventDefault();
+		$("ul", "section").fadeOut(150);
+		$("ul", this).fadeIn(150);
+	});
+
+	$("a","li").on("hover", function(e){
+		console.log(this);
 	});
 
 });
