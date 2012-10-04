e1471babfaa75b263cdff8468383a330b937d160
Kheng Hui Yeo <icedwater@gmail.com>
2012-10-04 22:54:07 +0800

    Tidying up without the build junk, ignored bin/*. Might add apks later.

--- SLeague/bin/SLeague-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/iced/experiment/SLeague/bin/SLeague-debug-unaligned.apk : \
-/home/iced/experiment/SLeague/bin/SLeague.ap_ \
-/home/iced/experiment/SLeague/bin/classes.dex \
-/home/iced/android-sdk-linux/tools/support/annotations.jar \
